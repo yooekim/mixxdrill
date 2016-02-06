46ddef2233efab86a804399d2ced1e1ad4eb3a22
Ryan Lagasse <lagasse.ryan.p@gmail.com>
2016-02-06 17:29:25 -0500

    First Push

---
+++ index.php
@@ -0,0 +1,24 @@
+!DOCTYPE html
+<html>
+
+<head>
+	<link href="css/main.js" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+	<header>
+
+	</header>
+
+	<section class="jumbotron">
+
+	</section>
+
+	<footer>
+
+	</footer>
+
+	<script src="js/script.js"></script>
+
+</body>
+</html>
